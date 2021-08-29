597c6062c170c110feccae0d4a76c13d187ad0be
KamWit7 <kamilw77@interia.pl>
2021-08-29 14:23:20 +0200

    :)

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,35 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const qs = (selector) => document.querySelector(selector);
+const gallery = qs(".gallery");
+
+
+function defaultGalery(){
+    const arratOfFalleryItems = galleryItems.map((ele) => {
+        const {preview, original, description} =  ele
+        const galleryItem = `  
+            <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+            </a>`
+        return galleryItem
+    })
+    gallery.innerHTML = arratOfFalleryItems.join('');
+} 
+
+
+defaultGalery()
+
+
+
+const  galleryLink = qs(".gallery__link")
+galleryLink.addEventListener("click",(e)=>{
+    e.preventDefault()
+    
+})
+console.log(galleryLink)
