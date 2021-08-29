02dc469986b95a681568fe5d6d68d17f0e8d8d68
KamWit7 <kamilw77@interia.pl>
2021-08-29 19:47:49 +0200

    :)

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js"
 // Change code below this line
 
-console.log(galleryItems);
+const createGalery = () => {
+  const galleryArray = galleryItems.map((ele) => {
+    const { preview, original, description } = ele
+    let galleryItem = `  
+            <a class="gallery__link" href="${original}">
+            <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+            />
+            </a>`
+    return galleryItem
+  })
+  return galleryArray.join("")
+}
+
+function defaultGalery() {
+  gallery.innerHTML = createGalery()
+  //   eveDelegationLightBox()
+  // lightBox()
+}
+console.log(galleryItems)
